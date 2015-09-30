37d40973fc6dad10d76e6b03ff2e0828a8459f4c
ExtremeLVL9001-DODO <ExtremeLVL9001-DODO@users.noreply.github.com>
2015-09-29 20:21:08 -0700

    Update PushBotManualArcade.java
    bug fix and added instant dance button (demo)
    
    (^_^)

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotManualArcade.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotManualArcade.java
@@ -26,6 +26,7 @@ public class PushBotManualArcade extends PushBotTelemetry
     // The system calls this member when the class is instantiated.
     //--------
     public PushBotManualArcade ()
+    float partytimer = 0
 
     {
         //
@@ -80,8 +81,24 @@ public class PushBotManualArcade extends PushBotTelemetry
         // thus capping drive speed to one
         // you will experience jankiness when you jump a 1 total in |x| + |y| as one wheel hits max speed
         // don't know how to solve that yet
+        // 
+        // update : realised there is a clip method, don't know how to use it though
 	    float minmaxleftdrive = Math.max(Math.min( leftStickY + leftStickX, 1), -1);
         float minmaxrightdrive = Math.max(Math.min( leftStickY - leftStickX, 1), -1);
+        
+        
+       // party button
+       // does randomised actions while 
+        
+        float partybutton = gamepad1.left_bumper;
+        if (partybutton==true){
+        	float partytimer += 1;
+        	if (partytimer == 40){
+        		partytimer = 0
+        		float movement = Math.rand()
+        		
+        	}
+        }
 
         //setting drive powers
         float l_left_drive_power = (float)scale_motor_power (minmaxleftdrive);
